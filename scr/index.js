a4e44723baa4f2572bc32af67130789d103cc9e4
crazy0codes <madhanpanja@gmail.com>
2023-02-22 22:03:03 +0530

    adding invalid inputs by using external JS

---
+++ scr/index.js
@@ -0,0 +1,22 @@
+let Card_Number = document.querySelector('.fcard_number');
+const Card_form = document.querySelector('form');
+const Card_Cvv = document.querySelector('#card_cvv');
+const Card_Month = document.querySelector('#card_m');
+const Card_Year_Error = document.querySelector('.card_m_y_error');
+const Card_Cvv_Error = document.querySelector('.card_cvv_error');
+const Card_Name = document.querySelector('.fcard_name');
+
+Card_Cvv.setCustomValidity("Hi");
+
+Card_form.addEventListener('input',function(){
+    Card_Name.value = Card_Name.value.toUpperCase();
+    if(Card_Month.value > 12 &&typeof Card_Month.value != 'Number'){
+        Card_Year_Error.innerHTML = 'not valid';
+    }
+    else {
+        Card_Year_Error.innerHTML = " ";
+    }
+    // if(Card_Number.value.length%4==0){
+    //     Card_Number.value += " ";
+    // }
+})
