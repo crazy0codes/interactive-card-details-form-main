08d05ad5758a8a41165a9c3f31b6c42816163969
crazy0codes <madhanpanja@gmail.com>
2023-02-25 14:48:29 +0530

    Added functionality to card's Name

--- scr/index.js
+++ scr/index.js
@@ -1,61 +1,64 @@
-let Card_Number = document.querySelector('.fcard_number');
-const Card_form = document.querySelector('form');
-const Card_Cvv = document.querySelector('#card_cvv');
-const Card_Month = document.querySelector('#card_m');
-const Card_Year_Error = document.querySelector('.card_m_y_error');
-const Card_Cvv_Error = document.querySelector('.card_cvv_error');
-const Card_Name = document.querySelector('.fcard_name');
+/* Taking variables  */
 
-Card_Cvv.setCustomValidity("Hi");
+const Card_form = document.querySelector('form'); // Form
+const Card_Name = document.querySelector('.fcard_name'); //Card's Name
+const Card_Number = document.querySelector('.fcard_number'); //Card's Number
+const Card_Month = document.querySelector('#card_m'); //Card's year and month
+const Card_Cvv = document.querySelector('#card_cvv'); //Card's CVV number
+
+/* Errors */
+
+const Card_Year_Error = document.querySelector('.card_m_y_error'); //Displaying the Error for Month and Year
+const Card_Cvv_Error = document.querySelector('.card_cvv_error');  //Displaying the Error for CVV
+const Card_Name_Error = document.querySelector('.card_name_error');
+
+/* Adding functionality to the form*/
 
 Card_form.addEventListener('input',function(){
-    Card_Name.value = Card_Name.value.toUpperCase(); //Name
-    if(Card_Month.value > 12 || typeof Card_Month.value === 'number'){
-        Card_Year_Error.innerHTML = 'not valid';
-    }
-    else {
-        Card_Year_Error.innerHTML = " ";
-    }
+
+//Card's Number dividing after 4 digits
+if(Card_Number.value == '\b'){
+    alert("User entered the BackSpace")
+}
+
 })
 
-var Name = "Madhan";
-console.log(
+// Card_Name.addEventListener('input',
+// function(){
+//    //Converting the Card's Name to UpperCase letters
+//    Card_Name.value = Card_Name.value.toUpperCase(); 
 
-(Cloned_obj.getFullName === obj.getFullName) ? true : false
+//    //Removing the xharacters that are not Letters
+//    let Card_Name_len = Card_Name.value.length;
+//    let latestLetter = Card_Name.value[Card_Name_len-1]
+//    let Check = function(latestLetter) {
+//     return /[A-Z]/.test(latestLetter);
+//   }
+//   let isLetter = Check(latestLetter);
+//   if (isLetter) {
+//     alert("It is a letter");
+//   } else {
+//     alert("It is not a letter");
+//   }
+//   })
 
-)
-const obj ={
-    LastName : "SDF",
-
-    getFullName() {
-        return this.LastName + " " + this[Name]
+Card_Name.addEventListener("input", checkCardName);
+function checkCardName(event) {
+    // Convert the card name to uppercase letters
+    let Card_Name = event.target;
+    let cardName = Card_Name.value.toUpperCase();
+    Card_Name.value = cardName;
+  
+    // Check if the card name contains only letters
+    let containsOnlyLetters = /^[A-Z]+$/.test(cardName);
+  
+    // Display an error message if the card name contains non-letter characters
+    if (!containsOnlyLetters) {
+      Card_Name_Error.innerText = "Invalid";
+    } else {
+      Card_Name_Error.innerText = "";
     }
-};
-
-
-obj[Name] = "FirstName"
-
-console.log(obj.getFullName());
-
-
-var Cloned_obj = Object.assign({},obj);
-
-console.log(
-
-(Cloned_obj.getFullName === obj.getFullName) ? true : false
-
-)
-
-Cloned_obj = structuredClone({},obj)
-
-console.log(
-
-    (Cloned_obj.getFullName === obj.getFullName) ? true : false
-    
-    )
-
-var a = confirm("Do you agree") ?? false;
-
-while(a != false){
-    a
-} 
+  }
+  
+  
+  
